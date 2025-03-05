cb2091e6d05ad6bc440f13d56b0b81f7eb4c28e4
Eeshan-vaghjiani <evaghjiani@gmail.com>
2025-03-05 14:37:40 +0300

    phonenumbers

--- new.py
+++ new.py
@@ -1,3 +1,24 @@
 import phonenumbers
 from phonenumbers import geocoder
+from test import number
 import folium
+
+key = "2f92da82d2164970a0b250df0098414f"
+
+check_number = phonenumbers.parse(number)
+number_location = geocoder.description_for_number(check_number,"en")
+print (number_location)
+
+
+from phonenumbers import carrier 
+service_provider = phonenumbers.parse(number)
+print(carrier.name_for_number(service_provider,"en"))
+
+from opencage.geocoder import OpenCageGeocode
+geocoder =OpenCageGeocode(key)
+
+query = str(number_location)
+results = geocoder.geocode(query)
+
+lat = results[0]['geometry']['lat']
+ 
